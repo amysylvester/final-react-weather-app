740cefdc768fd9c97293e29eec737e1f82223503
amysylvester <98198134+amysylvester@users.noreply.github.com>
2022-07-17 12:08:49 -0400

    Create & import Weather.js and .css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Amy Sylvester and is{" "}
           <a
