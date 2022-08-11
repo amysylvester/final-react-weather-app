79c92f057ceb1f7f654c59bd850941ce20fc85ad
amysylvester <98198134+amysylvester@users.noreply.github.com>
2022-08-11 18:50:41 -0400

    function test weatherforecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
-  return "This is the forecast";
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">Thu Icon 19 10</div>
+      </div>
+    </div>
+  );
 }
